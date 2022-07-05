bb6ff31cf274f13589bd13bbf2d49bce78224e3a
mooyoungoh <dhandand123@gmail.com>
2022-07-04 20:49:22 -0700

    Time: 1 ms (92.62%), Space: 43.8 MB (29.23%) - LeetHub

--- 256-paint-house/256-paint-house.java
+++ 256-paint-house/256-paint-house.java
@@ -1,38 +1,31 @@
 class Solution {
     public int minCost(int[][] costs) {
+        int m = costs.length;
+        int n = costs[0].length;
         
-  if(costs.length == 0 || costs[0].length == 0)
-        return 0;
-    int m = costs.length, n = costs[0].length;
-    
-    //initialize dp matrix
-    int[][] dp = new int[m][n];
-    for(int j = 0; j < n; j++) 
-        dp[0][j] = costs[0][j];
+        int[][] dp = new int[m][n];
         
-    //fill the dp matrix
-    for(int i = 1; i < m; i++) {
-        for(int j = 0; j < n; j++)
-            dp[i][j] = costs[i][j] + findMinInOneRow(i - 1, j, dp);
+        for(int j = 0; j < n; j++){
+            dp[0][j] = costs[0][j];
+        }
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 0 ; j < n; j++){
+                dp[i][j] = costs[i][j] + findMin(i-1, j, dp);
+            }
+        }
+        
+        return findMin(m-1, -1, dp);
     }
     
-    return findMinInOneRow(m - 1, -1, dp);
-}
-
-private int findMinInOneRow(int i, int j, int[][] matrix) {
-    int min = Integer.MAX_VALUE;
-    for(int k = 0; k < matrix[0].length; k++) {
-        if(k == j)
-            continue;
-        min = Math.min(min, matrix[i][k]);
+    private int findMin(int i, int j, int[][] matrix){
+        int min = Integer.MAX_VALUE;
+        for(int k = 0 ; k < matrix[0].length; k++){
+            if(k == j){
+                continue;
+            }
+            min = Math.min(min, matrix[i][k]);
+        }
+        return min;
     }
-    return min;
 }
-}
-
-/*
-res blue green
-
-
-*/
-
