cf8417455c99dd499bd588d5e921faeef0d01d4b
mooyoungoh <dhandand123@gmail.com>
2022-07-11 14:48:32 -0700

    Time: 38 ms (20.11%), Space: 43.7 MB (80.80%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.java
+++ 224-basic-calculator/224-basic-calculator.java
@@ -1,27 +1,28 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);                          // Always keep most recent sum at top
-        for (int i = 0, sign = 1; i < s.length(); i++) {
-            if (Character.isDigit(s.charAt(i))) {
-                int num = s.charAt(i) - '0';    // Be aware of outer loop boundary and i++
-                while(i < s.length() - 1 && Character.isDigit(s.charAt(i + 1))) {
-                    num = num * 10 + (s.charAt(i + 1) - '0');
+        Stack<Integer> stk = new Stack<>();
+        stk.push(0);
+        for(int i = 0, sign = 1; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(Character.isDigit(s.charAt(i))){
+                int num = s.charAt(i) - '0';
+                while(i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))){
+                    num = num * 10 + s.charAt(i+1) - '0';
                     i++;
                 }
-                stack.push(stack.pop() + sign * num);
-            } else if (s.charAt(i) == '+') {
+                stk.push(stk.pop() + sign * num);
+            }else if(c == '+'){
                 sign = 1;
-            } else if (s.charAt(i) == '-') {
+            }else if(c == '-'){
                 sign = -1;
-            } else if (s.charAt(i) == '(') {
-                stack.push(sign);
-                stack.push(0);
+            }else if(c == '('){
+                stk.push(sign);
+                stk.push(0);
                 sign = 1;
-            } else if (s.charAt(i) == ')') {    // Update last sum = current sum * sign
-                stack.push(stack.pop() * stack.pop() + stack.pop());
-            } 
+            }else if(c == ')'){
+                stk.push(stk.pop() * stk.pop() + stk.pop());
+            }
         }
-        return stack.pop();
+        return stk.pop();
     }
 }
