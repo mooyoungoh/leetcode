b9509130cc256bc148be0d7bb03813cc14c3d486
mooyoungoh <dhandand123@gmail.com>
2022-07-11 11:04:11 -0700

    Time: 61 ms (27.22%), Space: 186.4 MB (27.02%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.java
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] amountPainted(int[][] paint) {
-        int[] answer = new int[paint.length];
-        int[] arr = new int[50001];
-        for (int i = 0; i < paint.length; i++) {
+        int[] ans = new int[paint.length];
+        int[] canvas = new int[500001];
+        int idx = 0;
+        for(int[]p : paint){
             int count = 0;
-            int index = paint[i][0];
-            while (paint[i][1] > index) {
-                if (arr[index] == 0) {
-                    arr[index] = paint[i][1];
+            int index = p[0];
+            while(index < p[1]){
+                if(canvas[index] == 0){
+                    canvas[index] = p[1];
                     count++;
                     index++;
-                } else {
-                    index = arr[index];
+                }else{
+                    index = canvas[index];
                 }
             }
-            answer[i] = count;
+            ans[idx++] = count;
         }
-        return answer;
         
+        return ans;
     }
 }
