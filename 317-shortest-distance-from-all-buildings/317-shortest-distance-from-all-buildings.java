e5a9b32fa5b63f0977eb1bdde6aff425e584b7e5
mooyoungoh <dhandand123@gmail.com>
2022-07-13 11:15:45 -0700

    Time: 314 ms (64.28%), Space: 42.5 MB (89.50%) - LeetHub

--- 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.java
+++ 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.java
@@ -1,33 +1,32 @@
 class Solution {
-    
-    final int[][] DIRS = {{1,0}, {-1, 0}, {0,1}, {0,-1}};
-    
+    private final int[][] DIRS = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+
     public int shortestDistance(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][]dp = new int[m][n];
-        int[][]reach = new int[m][n];
+        int[][] dp = new int[m][n];
+        int[][] reach = new int[m][n];
+        int count = 0;
         Queue<int[]> q = new LinkedList<>();
-        int countBuilding = 0;
-        for(int i = 0; i < m; i++){
+        for(int i = 0 ; i < m ; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 1){
                     q.offer(new int[]{i,j});
-                    bfs(q, grid, dp, reach, m, n);
-                    countBuilding++;
+                    bfs(q,grid, dp, reach, m, n);
+                    count++;
                 }
             }
         }
-        //
+        
         int result = Integer.MAX_VALUE;
-        for(int i = 0 ; i < m; i++){
-            for(int j = 0 ; j < n; j++){
-                if(grid[i][j] == 0 && reach[i][j] == countBuilding){
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 0 && reach[i][j]==count){
                     result = Math.min(result, dp[i][j]);
                 }
             }
         }
-        return result == Integer.MAX_VALUE ? -1 : result;
+        return result == Integer.MAX_VALUE? -1 : result;
     }
     
     private void bfs(Queue<int[]> q, int[][] grid, int[][] dp, int[][] reach, int m, int n){
@@ -36,13 +35,13 @@ class Solution {
         while(!q.isEmpty()){
             int size = q.size();
             for(int i = 0 ; i < size; i++){
-                int[]cur = q.poll();
+                int[] cur = q.poll();
                 int x = cur[0];
                 int y = cur[1];
                 for(int[] dir : DIRS){
                     int dx = x + dir[0];
                     int dy = y + dir[1];
-                    if(dx < 0 || dx > m-1 || dy < 0 || dy > n-1 || grid[dx][dy] != 0 || visited[dx][dy]) 
+                    if(dx < 0 || dx > m-1 || dy < 0 || dy > n-1 || visited[dx][dy] || grid[dx][dy] !=0 )
                         continue;
                     q.offer(new int[]{dx, dy});
                     visited[dx][dy] = true;
@@ -53,5 +52,4 @@ class Solution {
             level++;
         }
     }
-    
 }
