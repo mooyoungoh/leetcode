3cf0af5e319e84425f15f8fe5258a65e910eaeff
mooyoungoh <dhandand123@gmail.com>
2022-07-13 15:25:22 -0700

    Time: 33 ms (86.98%), Space: 49.6 MB (31.11%) - LeetHub

--- 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.java
+++ 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.java
@@ -12,6 +12,10 @@ class Solution {
     public Node inorderSuccessor(Node node) {
         if(node == null)
             return null;
+        /*
+        If right subtree of node is not NULL, then succ lies in right subtree. Do the following. 
+            Go to right subtree and return the node with minimum key value in the right subtree.
+        */
         if(node.right != null){
             node = node.right;
             while(node != null && node.left != null){
@@ -19,6 +23,12 @@ class Solution {
             }
             return node;
         }
+        
+        /*
+        If right subtree of node is NULL, then succ is one of the ancestors. Do the following. 
+            Travel up using the parent pointer until you see a node which is left child of its parent. 
+            The parent of such a node is the succ.
+        */
         
         while(node != null){
             if(node.parent == null)
