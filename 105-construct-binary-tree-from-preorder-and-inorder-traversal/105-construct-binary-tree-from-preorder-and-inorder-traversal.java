62a0d7a960d011b296ba17e8b9dd3cddc572cc9a
mooyoungoh <dhandand123@gmail.com>
2022-07-14 02:12:43 -0700

    Time: 4 ms (64.48%), Space: 44 MB (68.90%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,26 +14,37 @@
  * }
  */
 class Solution {
-    
-    int index = 0;
+    int idx = 0;
     
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         return helper(preorder, inorder, 0, preorder.length-1);
     }
-    
-    private TreeNode helper(int[] preorder, int[] inorder, int start, int end){
-        if(start > end)return null;
+
+    private TreeNode helper(int[] preorder,int[] inorder, int start,int end){
+        if(start>end) return null;
         
-        TreeNode root = new TreeNode(preorder[index++]);
+        TreeNode root = new TreeNode(preorder[idx++]);
         
-        int i = 0;
+        int i=0;
+        
         while(inorder[i] != root.val){
             i++;
         }
-        
-        root.left = helper(preorder, inorder, start, i-1);
-        root.right = helper(preorder, inorder, i+1, end);
+        root.left = helper(preorder, inorder, start, i-1);  // left-side size 
+        root.right = helper(preorder, inorder, i+1, end);   // right-side size
         
         return root;
     }
 }
+
+/*
+
+# Preorder Traversal: Root -> Left -> Right
+# Inorder Traversal: Left -> Root -> Right
+# Hence, at any step, first value in the preorder traversal represents
+# root of the binary tree at that point.
+# If that value is at the i'th index in inorder list,
+# then all the values to its left are part of left subtree.
+# Similarly, all the values to its right are part of the right subtree.
+        
+*/
