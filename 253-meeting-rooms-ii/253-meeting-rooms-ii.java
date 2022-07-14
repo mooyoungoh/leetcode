c5d0c1d005bf8d2aa9b47e2122f72b0ec1babd83
mooyoungoh <dhandand123@gmail.com>
2022-07-14 12:03:20 -0700

    Time: 6 ms (86.38%), Space: 41.8 MB (98.58%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int[] interval : intervals){
+            if(pq.size() == 0){
+                pq.add(interval[1]);
+            }
+            else{
+                if(pq.peek()<=interval[0]){
+                    pq.poll();
+                }
+                pq.add(interval[1]);
+            }
+        }
+        return pq.size();
+    }
+}
