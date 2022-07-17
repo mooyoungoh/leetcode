c97c203ef3db64149de389479d0ce40a38936866
mooyoungoh <dhandand123@gmail.com>
2022-07-16 23:04:24 -0700

    Time: 0 ms (100.00%), Space: 42.3 MB (32.26%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -1,13 +1,18 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-           int[][] res = new int[n][n];
-        int left = 0, right = n - 1, top = 0, bottom = n - 1;
+        int[][]res = new int[n][n];
+        int top = 0;
+        int left = 0;
+        int bottom = n - 1;
+        int right = n - 1;
         int count = 0;
-        while (count < n * n) {
+        while(count < n*n){
             for (int i = left; i <= right && count < n*n; i++) res[top][i] = ++count;
             for (int i = top + 1; i < bottom && count < n*n; i++) res[i][right] = ++count;
-            for (int i = right; i >= left && count < n*n; i--) res[bottom][i] = ++count;
-            for (int i = bottom - 1; i > top && count < n*n; i--) res[i][left] = ++count;
+            if(count < n*n){
+                for (int i = right; i >= left && count < n*n; i--) res[bottom][i] = ++count;
+                for (int i = bottom - 1; i > top && count < n*n; i--) res[i][left] = ++count;
+            }
             left++;right--;top++;bottom--;
         }
         return res;
