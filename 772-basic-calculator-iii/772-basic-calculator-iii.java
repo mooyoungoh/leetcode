228f114f2c33eedd70c258c7f72b7d49add3ad54
mooyoungoh <dhandand123@gmail.com>
2022-07-16 20:06:13 -0700

    Time: 7 ms (55.97%), Space: 42.8 MB (37.70%) - LeetHub

--- 772-basic-calculator-iii/772-basic-calculator-iii.java
+++ 772-basic-calculator-iii/772-basic-calculator-iii.java
@@ -1,9 +1,7 @@
 class Solution {
     public int calculate(String s) {
-        int l1 = 0, o1 = 1; // Initialization of level one
-        int l2 = 1, o2 = 1; // Initialization of level two
-
-        Deque<Integer> stack = new ArrayDeque<>(); // stack to simulate recursion
+        int l1 = 0, o1 = 1;
+        int l2 = 1, o2 = 1;
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
@@ -18,23 +16,16 @@ class Solution {
                 l2 = (o2 == 1 ? l2 * num : l2 / num);
 
             } else if (c == '(') {
-                // First preserve current calculation status
-                stack.offerFirst(l1); stack.offerFirst(o1);
-                stack.offerFirst(l2); stack.offerFirst(o2);
+                int j = i;
 
-                // Then reset it for next calculation
-                l1 = 0; o1 = 1;
-                l2 = 1; o2 = 1;
+                for (int cnt = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == '(') cnt++;
+                    if (s.charAt(i) == ')') cnt--;
+                    if (cnt == 0) break;
+                }
 
-            } else if (c == ')') {
-                // First preserve the result of current calculation
-                int num = l1 + o1 * l2;
+                int num = calculate(s.substring(j + 1, i));
 
-                // Then restore previous calculation status
-                o2 = stack.poll(); l2 = stack.poll();
-                o1 = stack.poll(); l1 = stack.poll();
-
-                // Previous calculation status is now in effect
                 l2 = (o2 == 1 ? l2 * num : l2 / num);
 
             } else if (c == '*' || c == '/') {
