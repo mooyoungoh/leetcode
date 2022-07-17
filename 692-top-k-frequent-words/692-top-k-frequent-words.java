a8aca8eb84584c9c3a6d754b43406f650e8edd9d
mooyoungoh <dhandand123@gmail.com>
2022-07-17 16:00:45 -0700

    Time: 7 ms (86.13%), Space: 45.4 MB (42.99%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.java
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -7,29 +7,33 @@ class Solution {
             this.word = word;
             this.count = count;
         }
+        
     }
     
     public List<String> topKFrequent(String[] words, int k) {
         Map<String, WordCnt> map = new HashMap<>();
         for(String word: words){
-            if(!map.containsKey(word))
+            if(!map.containsKey(word)){
                 map.put(word, new WordCnt(word, 0));
-            else
+            }else{
                 map.get(word).count++;
+            }
         }
         
-        PriorityQueue<WordCnt> pq = new PriorityQueue<>(k, (a,b) -> a.count-b.count!=0 ? a.count-b.count : b.word.compareTo(a.word));
+        PriorityQueue<WordCnt> pq = new PriorityQueue<>(k, (a,b)-> a.count-b.count != 0 ? a.count-b.count : b.word.compareTo(a.word));
         
         for(WordCnt wordCnt : map.values()){
             pq.offer(wordCnt);
-            if(pq.size()>k)pq.poll();
+            if(pq.size() > k){
+                pq.poll();
+            }
         }
         
         List<String> ans = new ArrayList<>();
+        
         while(!pq.isEmpty()){
             ans.add(0, pq.poll().word);
         }
-        
         return ans;
     }
-}   
+}
