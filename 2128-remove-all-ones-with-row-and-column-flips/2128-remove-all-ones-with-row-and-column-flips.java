13508481fdaf5e6ed9a6d9eda4cb0401f819efae
mooyoungoh <dhandand123@gmail.com>
2022-07-17 23:28:04 -0700

    Time: 3 ms (41.39%), Space: 65.7 MB (39.19%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.java
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.java
@@ -3,44 +3,26 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         
-        for(int j = 0 ; j < n; j++){
+        for(int j = 0; j < n; j++){
             if(grid[0][j] == 1){
-                for(int i = 0 ; i < m; i++){
+                for(int i = 0; i < m; i++){
                     grid[i][j] = (grid[i][j] == 0) ? 1 : 0;
                 }
             }
         }
-
-        /*
-        for(int i = 1; i < m ; i++){
-            for(int j = 0; j < n-1; j++){
-                System.out.print(grid[i][j] + " " + grid[i][j+1]);
-                if(grid[i][j] != grid[i][j+1]){
-                    return false;
-                }
-            }
-        }
-        */
         
-        for(int i = 0; i < m; i++){
+        for(int i = 0 ; i < m; i++){
             int sum = 0;
-            for(int j=0; j < n; j++){
+            for(int j = 0; j < n; j++){
                 sum += grid[i][j];
             }
             
-            //System.out.println(sum);
             if(sum != 0 && sum != n){
                 return false;
             }
         }
         
+        
         return true;
     }
 }
-
-/*
-000
-111
-000
-
-*/
