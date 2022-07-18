b4ad1578e9d2c4d17eb8adf0cad2bd50a1eb728e
mooyoungoh <dhandand123@gmail.com>
2022-07-17 19:30:55 -0700

    Time: 412 ms (44.40%), Space: 201.9 MB (23.12%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.java
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.java
@@ -4,34 +4,33 @@ class Solution {
     
     public int minKnightMoves(int x, int y) {
         
-        x = Math.abs(x);
-        y = Math.abs(y);
-        
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.offer(new int[]{0,0});
-        
-        Set<String> set = new HashSet<>();
-        set.add("0,0");
-        int dist = 0;
-        
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i = 0 ; i < size; i++){
-                int[]cur = q.poll();
-                if(cur[0] == x && cur[1] == y) return dist;
-                for(int[] dir : DIRS){
-                    int nx = cur[0] + dir[0];
-                    int ny = cur[1] + dir[1];
-                    
-                    if(!set.contains(nx +","+ny) && nx >= -1 && nx>=-1){
-                        q.add(new int[]{nx, ny});
-                        set.add(nx+","+ny);
+        boolean[][] visited = new boolean[607][607];
+
+        Deque<int[]> queue = new LinkedList<>();
+        queue.addLast(new int[]{0, 0});
+        int steps = 0;
+
+        while (queue.size() > 0) {
+            int currLevelSize = queue.size();
+            // iterate through the current level
+            for (int i = 0; i < currLevelSize; i++) {
+                int[] curr = queue.removeFirst();
+                if (curr[0] == x && curr[1] == y) {
+                    return steps;
+                }
+
+                for (int[] offset : DIRS) {
+                    int[] next = new int[]{curr[0] + offset[0], curr[1] + offset[1]};
+                    // align the coordinate to the bitmap
+                    if (!visited[next[0] + 302][next[1] + 302]) {
+                        visited[next[0] + 302][next[1] + 302] = true;
+                        queue.addLast(next);
                     }
                 }
             }
-            dist++;
+            steps++;
         }
-        return -1;
+        // move on to the next level
+        return steps;
     }
 }
