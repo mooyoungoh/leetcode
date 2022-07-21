79ca52bce448c8fdc82fd6e1908e246a12dae57d
mooyoungoh <dhandand123@gmail.com>
2022-07-21 16:46:00 -0700

    Time: 20 ms (67.60%), Space: 43.2 MB (92.99%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,47 +1,16 @@
 class Solution {
-    public boolean winnerOfGame(String colors) {
-        int aCnt = 0;
-        int bCnt = 0;
+    public boolean winnerOfGame(String s) {
+        int a = 0, b = 0;
         
-        for(int i = 0; i < colors.length()-1;i++){
-            if(colors.charAt(i) == 'A'){
-                int j = i;
-                int tmp = 1;
-                while(j+1 < colors.length() && colors.charAt(j+1) == 'A'){
-                    j++;
-                    tmp++; 
-                    if(tmp >= 3){
-                        aCnt++;
-                    }
-                }
-                i = j;
-            }else if((colors.charAt(i) == 'B')){
-                int j = i;
-                int tmp = 1;
-                while(j+1< colors.length() && colors.charAt(j+1) == 'B'){
-                    j++;
-                    tmp++; 
-                    if(tmp >= 3){
-                        bCnt++;
-                    }
-                }
-                i = j;
-            }
+        for(int i=1; i<s.length()-1; i++){
+            if(s.charAt(i) == s.charAt(i-1) && s.charAt(i) == s.charAt(i+1)){
+                if(s.charAt(i) == 'A')
+                    a++;
+                else
+                    b++;
+			}
         }
-        
-        boolean ans = false;
-        if(aCnt == bCnt){
-            ans = false;
-        }else if(aCnt < bCnt){
-            ans = false;
-        }else{
-            ans = true;
-        }
-        
-        System.out.println(aCnt);
-        System.out.println(bCnt);
-        
-        return ans;
-        
+                    
+        return a>b;
     }
 }
