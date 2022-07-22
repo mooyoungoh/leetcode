6c76634b25c982790c128592343aa4385adb1f0d
mooyoungoh <dhandand123@gmail.com>
2022-07-22 12:07:45 -0700

    Time: 1056 ms (5.08%), Space: 178.3 MB (6.09%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.java
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.java
@@ -4,7 +4,7 @@ class Solution {
        Map<String, Integer> map = new HashMap<>();
        int largeLen = S.length() - 1;
        for(int len = largeLen; len >= 1; len--){
-           for(int i = 0; i + len - 1 < S.length(); i++){
+           for(int i = 0; i + len <= S.length(); i++){
                String cur = S.substring(i, i + len);
                map.put(cur, map.getOrDefault(cur, 0) + 1);
                if(map.get(cur) > 1){
