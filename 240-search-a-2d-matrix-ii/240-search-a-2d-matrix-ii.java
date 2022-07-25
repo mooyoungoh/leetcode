d3db56de017415e400e955f027946e221cf57f64
mooyoungoh <dhandand123@gmail.com>
2022-07-25 10:03:47 -0700

    Update 240-search-a-2d-matrix-ii.java

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,10 +1,16 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-                for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                if(matrix[i][j] == target) return true;
-            }
+        if(matrix == null || matrix.length == 0) return false;
+        
+        int col = matrix[0].length-1;
+        int row = 0;
+        
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] > target) col--;
+            else if(matrix[row][col] < target) row++;
         }
+        
         return false;
     }
 }
