5c59c0719b2cede4a7c16c93829cf11d0ff953da
mooyoungoh <dhandand123@gmail.com>
2022-08-03 14:57:26 -0700

    Time: 104 ms (24.31%), Space: 54.9 MB (28.22%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,15 +1,17 @@
 class MyCalendar {
 
+    List<int[]> books;
     
-    private List<int[]> books;
     public MyCalendar() {
         books = new ArrayList<>();
     }
     
     public boolean book(int start, int end) {
-        for (int[] b : books)
-            if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
-        books.add(new int[]{ start, end });
+        for(int[] b : books){
+            if(Math.max(b[0], start) < Math.min(b[1], end))
+                return false;
+        }
+        books.add(new int[]{start, end});
         return true;
     }
 }
