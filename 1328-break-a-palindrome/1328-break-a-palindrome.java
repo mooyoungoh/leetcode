026e74c68a15cb82c2720ce9e24589c37505ac0f
mooyoungoh <dhandand123@gmail.com>
2023-04-30 17:17:40 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (45.67%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String breakPalindrome(String p) {
+        char[] c = p.toCharArray();
+        for(int i = 0 ; i < c.length / 2; i++){
+            if(c[i] != 'a'){
+                c[i] = 'a';
+                return String.valueOf(c);
+            }
+        }
+        c[c.length-1] = 'b';
+        return c.length == 1 ? "" : String.valueOf(c);
+    }
+}
