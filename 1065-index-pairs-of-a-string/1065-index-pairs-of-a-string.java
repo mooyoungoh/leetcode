565fad2b4702a46712b94c4e8a4af82ca3e20579
mooyoungoh <dhandand123@gmail.com>
2023-05-02 19:30:32 -0700

    Time: 2 ms (99.30%), Space: 43.1 MB (34.84%) - LeetHub

---
+++ 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] indexPairs(String text, String[] words) {
+        List<int[]> res = new ArrayList<>();
+        for(int i = 0 ; i < words.length; i++){
+            String temp = text;
+            int index = 0;
+            while(index != -1){
+                index = text.indexOf(words[i], index);
+                if(index != -1){
+                    res.add(new int[] {index, index+words[i].length() -1});
+                    index++;
+                }
+            }
+        }
+        Collections.sort(res, (a,b) -> a[0] == b[0]? a[1] - b[1] : a[0] - b[0]);
+        return res.toArray(new int[0][]);
+    }
+}
