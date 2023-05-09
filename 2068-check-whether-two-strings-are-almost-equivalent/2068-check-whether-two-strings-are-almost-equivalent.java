ca92e32fb442134831bd0b61cc4dc7aa88c5ff64
mooyoungoh <dhandand123@gmail.com>
2023-05-08 17:10:14 -0700

    Time: 1 ms (96.58%), Space: 41.1 MB (40.35%) - LeetHub

---
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        int a[] = new int[26];
+        int b[] = new int[26];
+        for(int i = 0 ; i < word1.length(); i++){
+            a[word1.charAt(i) - 'a']++;
+            b[word2.charAt(i) - 'a']++;
+        }
+        
+        for(int i = 0 ; i < 26; i++){
+            if(Math.abs(a[i] - b[i]) > 3)
+                return false;
+        }
+        return true;
+    }
+}
