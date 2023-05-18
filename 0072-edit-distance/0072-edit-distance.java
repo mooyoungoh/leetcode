8608fa58a6110c6e51548575a4e72abb8805a6de
mooyoungoh <dhandand123@gmail.com>
2023-05-18 11:07:10 -0700

    Time: 7 ms (26.37%), Space: 43.7 MB (7.63%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,27 +1,27 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        
         int m = word1.length();
         int n = word2.length();
-        int [][]d = new int[m + 1][n + 1];
-        for(int i = 0 ; i <= m; i++){
-            d[i][0] = i; 
+        int [][]dp = new int[m+1][n+1];
+        for(int i = 0; i <= m; i++){
+            dp[i][0] = i;
         }
-        for(int i = 0 ; i <= n; i++){
-            d[0][i] = i;
+        for(int i = 0; i <= n; i++){
+            dp[0][i] = i;
         }
+        
         for(int i = 1; i <= m; i++){
-            for(int j = 1; j <=n; j++){
+            for(int j = 1; j <= n; j++){
                 if(word1.charAt(i - 1) == word2.charAt(j - 1)){
-                    d[i][j] = d[i-1][j-1];
+                    dp[i][j] = dp[i-1][j-1];
                 }else{
-                    int delete = d[i-1][j];
-                    int insert = d[i][j-1];
-                    int sub = d[i-1][j-1];
-                    d[i][j] = Math.min(delete, Math.min(insert, sub)) + 1;
+                    int delete = dp[i-1][j];
+                    int insert = dp[i][j-1];
+                    int replace = dp[i-1][j-1];
+                    dp[i][j] = Math.min(delete, Math.min(insert, replace)) + 1;
                 }
             }
         }
-        return d[m][n];
+        return dp[m][n];
     }
 }
