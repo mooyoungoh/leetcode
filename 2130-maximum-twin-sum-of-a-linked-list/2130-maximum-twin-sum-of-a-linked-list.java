65fad10b9ce3c83c0f55b424032c97f3fc1030a0
mooyoungoh <dhandand123@gmail.com>
2023-05-23 14:17:46 -0700

    Time: 8 ms (50.41%), Space: 62.3 MB (60.03%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -10,38 +10,40 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        ListNode middle = half(head);
-        ListNode rev = reverse(middle);
+        ListNode walker = middle(head);
+        ListNode flip = reverse(walker);
         
         int ans = 0;
         
-        while(head != null && rev != null){
-            ans = Math.max(head.val + rev.val, ans);
+        while(head != null && flip != null){
+            ans = Math.max(ans, head.val + flip.val);
             head = head.next;
-            rev = rev.next;
+            flip = flip.next;
         }
-        return ans; 
+        return ans;
     }
+    
+    private ListNode middle(ListNode node){
+        ListNode walker = node;
+        ListNode runner = node;
+        while(runner.next != null){
+            runner = runner.next;
+            walker = walker.next;
+            if(runner.next != null){
+                runner = runner.next;
+            }
+        }
+        return walker;
+    }
+    
     private ListNode reverse(ListNode head){
         ListNode prev = null;
-        while(head!=null){
+        while(head != null){
             ListNode next = head.next;
             head.next = prev;
             prev = head;
             head = next;
         }
         return prev;
-    }
-    private ListNode half(ListNode head){
-        ListNode walker = head;
-        ListNode runner = head;
-        while(runner != null){
-            runner = runner.next;
-            walker = walker.next;
-            if(runner != null){
-                runner = runner.next;
-            }
-        }
-        return walker;
     }
 }
