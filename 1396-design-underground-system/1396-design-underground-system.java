f720b679a2e508a0707730b176c7ee057c20600a
mooyoungoh <dhandand123@gmail.com>
2023-05-31 12:57:58 -0700

    Time: 109 ms (20.33%), Space: 56.5 MB (20.56%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,29 +1,29 @@
 class UndergroundSystem {
+    
+    Map<Integer, Pair<String, Integer>> checkIn = new HashMap<>();
+    Map<String, Pair<Double, Integer>> route = new HashMap<>();
 
-    Map<Integer, Pair<String, Integer>> checkInMap = new HashMap<>();  // Uid - {StationName, Time}
-    Map<String, Pair<Double, Integer>> routeMap = new HashMap<>(); // RouteName - {TotalTime, Count}
-
-    public UndergroundSystem() {}
-
+    public UndergroundSystem() {
+        
+    }
+    
     public void checkIn(int id, String stationName, int t) {
-        checkInMap.put(id, new Pair<>(stationName, t));
+        checkIn.put(id, new Pair<>(stationName, t));
     }
-
+    
     public void checkOut(int id, String stationName, int t) {
-        Pair<String, Integer> checkIn = checkInMap.get(id);
-        checkInMap.remove(id); // Remove after using it which will not make HashTable big
-
-        String routeName = checkIn.getKey() + "_" + stationName;
-        int totalTime = t - checkIn.getValue();
-
-        Pair<Double, Integer> route = routeMap.getOrDefault(routeName, new Pair<>(0.0, 0));
-        routeMap.put(routeName, new Pair<>(route.getKey() + totalTime, route.getValue() + 1));
+        Pair<String, Integer> cur = checkIn.get(id);
+        String routeName = cur.getKey() + "_" + stationName;
+        int totalTime = t - cur.getValue();
+        
+        Pair<Double, Integer> rou = route.getOrDefault(routeName, new Pair(0.0, 0));
+        route.put(routeName, new Pair<>(rou.getKey() + totalTime, rou.getValue() + 1));
     }
-
+    
     public double getAverageTime(String startStation, String endStation) {
         String routeName = startStation + "_" + endStation;
-        Pair<Double, Integer> trip = routeMap.get(routeName);
-        return trip.getKey() / trip.getValue();
+        Pair<Double, Integer> cur = route.get(routeName);
+        return cur.getKey() / cur.getValue();
     }
 }
 
