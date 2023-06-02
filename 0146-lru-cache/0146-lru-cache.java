4e77c0157979e9c7383f2a6bba0947fa8dd20812
mooyoungoh <dhandand123@gmail.com>
2023-06-02 14:28:57 -0700

    Time: 64 ms (75.19%), Space: 120.7 MB (21.30%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,11 +1,11 @@
 class LRUCache {
-    
+
     public class CacheItem{
         int key;
-        int value;
-        public CacheItem(int key, int value){
+        int val;
+        public CacheItem(int key, int val){
             this.key = key;
-            this.value = value;
+            this.val = val;
         }
         CacheItem prev;
         CacheItem next;
@@ -13,9 +13,9 @@ class LRUCache {
     
     CacheItem head;
     CacheItem tail;
-    Map<Integer, CacheItem> map;
     int capacity;
-
+    Map<Integer, CacheItem> map;
+    
     public LRUCache(int capacity) {
         head = null;
         tail = null;
@@ -25,26 +25,29 @@ class LRUCache {
     
     public int get(int key) {
         if(!map.containsKey(key)) return -1;
+        
         CacheItem cur = map.get(key);
         if(cur != head){
             if(cur == tail){
                 tail = tail.prev;
             }
-            // head - cur.prev - cur - cur.next
+            // head - cur.prev - cur - cur.next;
             if(cur.prev != null) cur.prev.next = cur.next;
             if(cur.next != null) cur.next.prev = cur.prev;
             
             head.prev = cur;
             cur.next = head;
             head = cur;
-        }        
-        return cur.value;
+        }
+        
+        return cur.val;
     }
     
     public void put(int key, int value) {
         if(get(key) == -1){
-            //insert
+            //new
             CacheItem cur = new CacheItem(key, value);
+            
             if(head == null){
                 head = cur;
                 tail = cur;
@@ -53,15 +56,17 @@ class LRUCache {
                 cur.next = head;
                 head = cur;
             }
+            
             map.put(key, cur);
+            
             if(map.size() > capacity){
                 map.remove(tail.key);
                 tail = tail.prev;
             }
-
+            
+            
         }else{
-            //update
-            map.get(key).value = value;
+            map.get(key).val = value;   
         }
     }
 }
