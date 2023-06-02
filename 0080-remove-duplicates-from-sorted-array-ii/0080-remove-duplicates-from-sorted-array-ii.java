c176d9ceef28e8705f37335257674d85257cf773
mooyoungoh <dhandand123@gmail.com>
2023-06-02 11:55:18 -0700

    Time: 0 ms (100.00%), Space: 43.9 MB (23.26%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,7 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int index = 1;
-        int k = 1;
         boolean check = false;
         for(int i = 1; i < nums.length;){
             while(i < nums.length && nums[i] == nums[i-1]){
@@ -10,9 +9,8 @@ class Solution {
             }
             if(check)
                 i--;
-            nums[index++] = nums[i];
+            nums[index++] = nums[i++];
             check = false;
-            i++;
         }
         return index;
     }
