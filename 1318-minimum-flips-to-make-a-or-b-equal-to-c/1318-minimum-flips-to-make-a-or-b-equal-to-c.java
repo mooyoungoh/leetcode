45c872430ab1a185c12bac6226ee0d4aabfb739d
mooyoungoh <dhandand123@gmail.com>
2023-06-07 11:36:13 -0700

    Time: 0 ms (100.00%), Space: 39.2 MB (80.07%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,14 +1,25 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
-        int ab = a | b;
-        int xor = ab ^ c;
         int ans = 0;
+        int ab = a | b;
+        int equal = ab ^ c;
         for(int i = 0; i < 32; i++){
             int mask = 1 << i;
-            if((xor & mask) > 0){
+            if((equal & mask) > 0){
                 ans += (a & mask) == (b & mask) && (c & mask) == 0 ? 2 : 1;
             }
         }
         return ans;
     }
 }
+
+
+
+    
+/*
+ 0010 0111
+ 0110 0010
+ 0110 0111
+ 
+ 0101 
+*/
