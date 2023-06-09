59fb692c612429ee8a6485d0039431fa9e7ce862
mooyoungoh <dhandand123@gmail.com>
2023-06-09 11:53:18 -0700

    Time: 47 ms (55.44%), Space: 50.8 MB (38.71%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,7 +5,7 @@ class Solution {
         
         Set<List<Integer>> set = new HashSet<>();
         
-        for(int i = 0; i < nums.length - 2; i++){
+        for(int i = 0; i < nums.length-2; i++){
             if(nums[i] > 0) continue;
             if(i > 0 && nums[i] == nums[i-1]) continue;
             int j = i + 1;
@@ -13,7 +13,7 @@ class Solution {
             while(j < k){
                 int sum = nums[i] + nums[j] + nums[k];
                 if(sum == 0){
-                    set.add(Arrays.asList(nums[i], nums[j],nums[k]));
+                    set.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
                 }else if(sum < 0){
