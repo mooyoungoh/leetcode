acac5b58b38a731f8ae814a5c3b0bbc68efe3b17
mooyoungoh <dhandand123@gmail.com>
2023-06-14 20:36:08 -0700

    Time: 44 ms (67.34%), Space: 48.5 MB (74.11%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,32 +1,22 @@
 class Solution {
-    public int equalPairs(int[][] g) {
-
-        int n = g.length;
-        int res = 0;
-		//convert the numbers in each row to a single string
-        Map<String, Integer> sToFreq = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; j++) { 
-                sb.append(g[i][j]);
-                sb.append("+");
+    public int equalPairs(int[][] grid) {
+        int count = 0;
+        int n = grid.length;
+        
+        for(int r = 0; r < n; r++){
+            for(int c = 0; c < n; c++){
+                boolean match = true;
+                
+                for(int i = 0; i < n; i++){
+                    if(grid[r][i] != grid[i][c]){
+                        match = false;
+                        break;
+                    }
+                }
+                count += match ? 1 : 0;
             }
-            String s = sb.toString();
-            sToFreq.put(s, sToFreq.getOrDefault(s, 0) + 1);
         }
         
-		//convert the numbers in each col to string and find the matches
-        for (int j = 0; j < n; j++) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < n; i++) { 
-                sb.append(g[i][j]);
-                sb.append("+");
-            }
-            String s = sb.toString();
-            if (sToFreq.containsKey(s)) {
-                res += sToFreq.get(s);
-            }
-        }
-        return res;
+        return count;
     }
 }
