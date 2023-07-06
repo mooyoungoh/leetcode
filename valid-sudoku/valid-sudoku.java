a95d5ae9e6e8f99e7a0c6782a95036b320fc357a
mooyoungoh <dhandand123@gmail.com>
2023-07-06 10:02:26 -0700

    Time: 2 ms (86.94%) | Memory: 43.4 MB (62.52%) - LeetSync

--- valid-sudoku/valid-sudoku.java
+++ valid-sudoku/valid-sudoku.java
@@ -3,7 +3,7 @@ class Solution {
         boolean[] b = new boolean[9];
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 9; j++){
-                Arrays.fill(b, false);
+               Arrays.fill(b, false);  
                 for(int k = 0; k < 9; k++){
                     char cur = '.';
                     if(i == 0){
@@ -11,10 +11,10 @@ class Solution {
                     }else if(i == 1){
                         cur = board[k][j];
                     }else{
-                        cur = board[j / 3 * 3 + k /3][j % 3 * 3 + k % 3];
+                        cur = board[j / 3 * 3 + k / 3][j % 3 * 3 + k % 3];
                     }
                     if(cur == '.') continue;
-                    int val = cur-'0';
+                    int val = cur - '0';
                     if(b[val-1]) return false;
                     b[val-1] = true;
                 }
