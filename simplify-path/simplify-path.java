74016a656fdba27b93b45034912141f780219885
mooyoungoh <dhandand123@gmail.com>
2023-07-07 10:23:25 -0700

    Time: 4 ms (92.77%) | Memory: 43.4 MB (62.47%) - LeetSync

--- simplify-path/simplify-path.java
+++ simplify-path/simplify-path.java
@@ -1,22 +1,21 @@
 class Solution {
     public String simplifyPath(String path) {
-        Stack<String> stack = new Stack<>();
+        Stack<String> stk = new Stack<>();
         for(String s : path.split("/")){
-            if(s.length() == 0 || s.equals("."))
+            if(s.length() == 0 || s.equals(".")){
                 continue;
-            else if(s.equals("..")){
-                if(!stack.isEmpty())
-                    stack.pop();
+            }else if(s.equals("..")){
+                if(!stk.isEmpty())
+                    stk.pop();
             }else{
-                stack.add(s);
+                stk.add(s);
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for(String s : stack)
-            sb.append("/").append(s);
-        
-
-        return sb.length() == 0 ? "/" : sb.toString();
+        StringBuilder s = new StringBuilder();
+        for(String ss : stk){
+            s.append("/").append(ss);
+        }
+        return s.length() == 0 ? "/" : s.toString();
     }
 }
