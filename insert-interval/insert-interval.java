e99d78737659a9f7b0b40bed710db18dc6b1f4eb
mooyoungoh <dhandand123@gmail.com>
2023-07-07 10:41:38 -0700

    Time: 1 ms (98.52%) | Memory: 44.1 MB (67.32%) - LeetSync

--- insert-interval/insert-interval.java
+++ insert-interval/insert-interval.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> ans = new ArrayList<>();
-        
+
         for(int[] slot : intervals){
             if(newInterval == null || slot[1] < newInterval[0]){
                 ans.add(slot);
@@ -10,13 +10,14 @@ class Solution {
                 ans.add(slot);
                 newInterval = null;
             }else{
-                newInterval[0] = Math.min(newInterval[0], slot[0]);
-                newInterval[1] = Math.max(newInterval[1], slot[1]);
+                newInterval[0] = Math.min(slot[0], newInterval[0]);
+                newInterval[1] = Math.max(slot[1], newInterval[1]);
             }
+
         }
+        
         if(newInterval != null)
             ans.add(newInterval);
-
         return ans.toArray(new int[ans.size()][]);
     }
 }
