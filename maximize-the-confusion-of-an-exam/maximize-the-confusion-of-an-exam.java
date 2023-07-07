1e8b2205bbe43c70f1841018bee14841bc5765fb
mooyoungoh <dhandand123@gmail.com>
2023-07-07 09:31:10 -0700

    Time: 16 ms (68.44%) | Memory: 43.7 MB (65.00%) - LeetSync

--- maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
+++ maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
@@ -2,12 +2,20 @@ class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
         int maxCount = 0;
         int maxLength = 0;
-        int[] freq = new int[26];
+        int countT = 0;
+        int countF = 0;
         for(int left = 0, right = 0; right < answerKey.length(); right++){
-            maxCount = Math.max(maxCount, ++freq[answerKey.charAt(right) - 'A']);
-            if(right - left + 1 - maxCount > k){
-                freq[answerKey.charAt(left) - 'A']--;
-                left++;
+            if(answerKey.charAt(right) == 'T')
+                countT++;
+            else
+                countF++;
+
+            while(Math.min(countT, countF) > k){
+                if(answerKey.charAt(left++) == 'T'){
+                    countT--;
+                }else{
+                    countF--;
+                }
             }
             maxLength = Math.max(maxLength, right - left + 1);
         }
