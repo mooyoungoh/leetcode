07c956832accab7f1ddad27f8124fb04e2b30710
mooyoungoh <dhandand123@gmail.com>
2023-07-10 09:07:42 -0700

    Time: 1 ms (99.26%) | Memory: 61.3 MB (88.06%) - LeetSync

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -23,7 +23,6 @@ class Solution {
             int size = q.size();
             for(int i = 0; i < size; i++){
                 TreeNode node = q.poll();
-                if(node == null) continue;
                 if(node.left == null && node.right == null) return ans;
                 if(node.left != null) q.offer(node.left);
                 if(node.right != null) q.offer(node.right);
