e655705d1012c9c318ede646148fc51e464e2f3e
mooyoungoh <dhandand123@gmail.com>
2023-07-10 14:44:45 -0700

    Time: 0 ms (100.00%) | Memory: 43.6 MB (27.58%) - LeetSync

--- path-sum/path-sum.java
+++ path-sum/path-sum.java
@@ -18,6 +18,6 @@ class Solution {
         if(root == null) return false;
         targetSum -= root.val;
         if(root.left == null && root.right == null) return targetSum == 0;
-        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);     
     }
 }
