26fe699711ed09f538a3c1484c822dfda3898ff3
mooyoungoh <dhandand123@gmail.com>
2023-07-12 10:20:00 -0700

    Time: 1 ms (91.18%) | Memory: 44.4 MB (61.06%) - LeetSync

--- binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -13,35 +13,19 @@
  *     }
  * }
  */
-
 class Solution {
+    int maxSum;
     public int maxPathSum(TreeNode root) {
         maxSum = Integer.MIN_VALUE;
-        gainFromSubtree(root);
+        dfs(root);
         return maxSum;
     }
+    public int dfs(TreeNode root){
+        if(root == null) return 0;
 
-    private int maxSum;
-
-    // post order traversal of subtree rooted at `root`
-    private int gainFromSubtree(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
-        // add the path sum from left subtree. Note that if the path
-        // sum is negative, we can ignore it, or count it as 0.
-        // This is the reason we use `Math.max` here.
-        int gainFromLeft = Math.max(gainFromSubtree(root.left), 0);
-
-        // add the path sum from right subtree. 0 if negative
-        int gainFromRight = Math.max(gainFromSubtree(root.right), 0);
-
-        // if left or right path sum are negative, they are counted
-        // as 0, so this statement takes care of all four scenarios
-        maxSum = Math.max(maxSum, gainFromLeft + gainFromRight + root.val);
-
-        // return the max sum for a path starting at the root of subtree
-        return Math.max(gainFromLeft + root.val, gainFromRight + root.val);
+        int left = Math.max(dfs(root.left), 0);
+        int right = Math.max(dfs(root.right), 0);
+        maxSum = Math.max(left + right + root.val, maxSum);
+        return Math.max(left + root.val, right + root.val);
     }
 }
