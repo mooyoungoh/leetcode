892b491ba9102368539026e36f7e83251aa7d23b
mooyoungoh <dhandand123@gmail.com>
2023-07-24 12:55:08 -0700

    Time: 2 ms (63.52%) | Memory: 40.7 MB (32.70%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,17 +1,13 @@
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
-
-        int hIndex = 0;
-        for (int i = citations.length - 1; i >= 0; i--) {
-            int numOfPapers = citations.length - i;
-            if (citations[i] >= numOfPapers) {
-                hIndex = numOfPapers;
-            } else {
-                break;
+        int ans = 0;
+        for(int i = citations.length - 1; i >=0 ; i--){
+            int paper = citations.length - i;
+            if(citations[i] >= paper){
+                ans = paper;
             }
         }
-
-        return hIndex;
+        return ans;
     }
 }
