9356c2acccbbc33fa65ceb073e39e33a67448b19
mooyoungoh <dhandand123@gmail.com>
2023-08-23 15:46:45 -0700

    Time: 0 ms (100.00%) | Memory: 39.4 MB (87.38%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -1,11 +1,11 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder ans = new StringBuilder();
         while(columnNumber > 0){
             columnNumber--;
-            sb.append((char)(columnNumber % 26 + 'A'));
+            ans.append((char)(columnNumber % 26 + 'A'));
             columnNumber /= 26;
         }
-        return sb.reverse().toString();
+        return ans.reverse().toString();
     }
 }
