955a0db3746427b879f9979c210c0af92f614da3
mooyoungoh <dhandand123@gmail.com>
2023-08-23 16:02:08 -0700

    Time: 62 ms (55.91%) | Memory: 44 MB (73.72%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -3,15 +3,15 @@ class Solution {
         int n = s.length();
         for(int i = 1; i < n; i++){
             if(n % i == 0){
-                StringBuilder pattern = new StringBuilder();
+                StringBuilder sb = new StringBuilder();
                 for(int j = 0; j < n / i; j++){
-                    pattern.append(s.substring(0, i));
+                    sb.append(s.substring(0, i));
                 }
-                if(s.equals(pattern.toString())){
+                if(s.equals(sb.toString()))
                     return true;
-                }
             }
         }
+
         return false;
     }
 }
