5f935321599310edb148dc29f06d52ffc487f806
mooyoungoh <dhandand123@gmail.com>
2023-08-23 16:08:38 -0700

    Time: 60 ms (58.44%) | Memory: 44 MB (59.68%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         int n = s.length();
-        for(int i = 1; i < n; i++){
+        for(int i = 1; i <= n / 2; i++){
             if(n % i == 0){
                 StringBuilder sb = new StringBuilder();
                 for(int j = 0; j < n / i; j++){
