dbce91f5edb65ca0d5f20e1c346f9def933d9f04
mooyoungoh <dhandand123@gmail.com>
2023-09-13 13:38:57 -0700

    Time: 3 ms (79.71%) | Memory: 43.7 MB (64.21%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -3,24 +3,26 @@ class Solution {
         int ans = 0;
         int[] left = new int[ratings.length];
         int[] right = new int[ratings.length];
-        
+
         Arrays.fill(left, 1);
         Arrays.fill(right, 1);
 
         for(int i = 1; i < ratings.length; i++){
-            if(ratings[i] > ratings[i-1]){
-                left[i] = left[i-1] + 1;
-            }
+            if(ratings[i - 1] < ratings[i]){
+                left[i] = left[i - 1] + 1;
+            } 
         }
 
         for(int i = ratings.length - 2; i >= 0; i--){
-            if(ratings[i] > ratings[i + 1])
-                right[i] = right[i+1] + 1;
+            if(ratings[i + 1] < ratings[i]){
+                right[i] = right[i + 1] + 1;
+            } 
         }
 
         for(int i = 0; i < ratings.length; i++){
-            ans+= Math.max(left[i], right[i]);
+            ans += Math.max(left[i], right[i]);
         }
+    
         return ans;
     }
 }
