e4f7cdae4ec68c0d32964b0629046c1219fd5d3e
mooyoungoh <dhandand123@gmail.com>
2023-11-01 14:39:52 -0700

    Time: 7 ms (32.13%) | Memory: 45.1 MB (12.11%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -14,36 +14,33 @@
  * }
  */
 class Solution {
-    
     Map<Integer, Integer> map = new HashMap<>();
     public int[] findMode(TreeNode root) {
-        tre(root);
+        recursion(root);
         int maxFreq = 0;
         for(int key : map.keySet()){
             maxFreq = Math.max(maxFreq, map.get(key));
-        }
+        }        
 
         List<Integer> ans = new ArrayList<>();
         for(int key : map.keySet()){
-            if(map.get(key) == maxFreq){
+            if(maxFreq == map.get(key)){
                 ans.add(key);
             }
+        }        
+        int[] res = new int[ans.size()];
+        for(int i = 0; i < ans.size(); i++){
+            res[i] = ans.get(i);
         }
 
-        int[] result = new int[ans.size()];
-        for(int i = 0; i < ans.size(); i++){
-            result[i] = ans.get(i);
-        }
-        return result;
+        return res;
     }
 
-    public void tre(TreeNode root){
+    public void recursion(TreeNode root){
         if(root == null) return;
-
-        
-        
-        if(root.left != null) tre(root.left);
         map.put(root.val, map.getOrDefault(root.val, 0) + 1);
-        if(root.right != null) tre(root.right);
-    } 
+        if(root.left != null) recursion(root.left);
+        if(root.right != null) recursion(root.right);
+        
+    }
 }
