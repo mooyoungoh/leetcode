ef500a988c24e054ebb265605b9f917562e20fc2
mooyoungoh <dhandand123@gmail.com>
2023-12-20 12:26:43 -0700

    Time: 2 ms (77.87%) | Memory: 44.3 MB (5.53%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+
+        return prices[0] + prices[1] > money ? money : money - prices[0] - prices[1] ;
+    }
+}
