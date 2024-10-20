66cc95e57164cbe98c413648a08ea44167585220
mooyoungoh <dhandand123@gmail.com>
2024-10-20 01:38:31 -0700

    Time: 2 ms (58.64%), Space: 41.7 MB (18.78%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length() <= 1)return false;
         Stack<Character> stk = new Stack<>();
-        char[] ac = s.toCharArray();
-        for(char c : ac){
-            if(c == '(' || c == '[' || c == '{')
-                stk.push(c);
+        char[] ca = s.toCharArray();
+        for(char c : ca){
+            if(c == '(') stk.push(')');
+            else if(c == '[') stk.push(']');
+            else if(c == '{') stk.push('}');
             else{
-                if(stk.empty())return false;
-                if(c == ')' && stk.pop() != '(') return false;
-                if(c == ']' && stk.pop() != '[') return false;
-                if(c == '}' && stk.pop() != '{') return false;
+                if(stk.isEmpty()) return false;
+                if(c != stk.pop()) return false;
             }
         }
+
         return stk.isEmpty();
     }
 }
