19bd12fcafaa453479292e143d9a5e8f483ceec5
mooyoungoh <dhandand123@gmail.com>
2025-05-04 23:46:50 -0700

    Time: 3 ms (98.57%), Space: 44.3 MB (71.73%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -8,6 +8,7 @@ class Solution {
         // Corresponding values for each Roman numeral symbol
         int[] values = {1000, 900, 500, 400, 100, 90, 
                         50, 40, 10, 9, 5, 4, 1};
+       
         int i = 0;
         while(num > 0){
             while(num >= values[i]){
