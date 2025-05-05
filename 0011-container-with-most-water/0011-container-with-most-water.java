649ce1e9e42dcbb1537ad9bfeee6c4dcabfc96f5
mooyoungoh <dhandand123@gmail.com>
2025-05-04 23:06:12 -0700

    Time: 5 ms (75.1%), Space: 58.3 MB (13.73%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -4,18 +4,16 @@ class Solution {
         int right = height.length - 1;
         int ans = 0;
         while(left <= right){
-            int shorterH = Math.min(height[left], height[right]);
-            int vh = (right - left) * shorterH;
+            int shoterHeight = Math.min(height[left], height[right]);
+            int vh = (right - left) * shoterHeight;
+            ans = Math.max(vh, ans);
 
             if(height[left] < height[right]){
                 left++;
             }else{
                 right--;
             }
-
-            ans = Math.max(ans, vh);
         }
-
         return ans;
     }
 }
