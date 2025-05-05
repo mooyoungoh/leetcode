bbec00b90875598ad840560bec7c2ac41fa03e19
mooyoungoh <dhandand123@gmail.com>
2025-05-04 23:12:13 -0700

    Time: 3 ms (98.57%), Space: 44.3 MB (71.73%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,6 +1,5 @@
 class Solution {
     public String intToRoman(int num) {
-        if (num < 1 || num > 3999) return "";
 	
         StringBuilder result = new StringBuilder();
 
@@ -8,9 +7,8 @@ class Solution {
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
         int i = 0;
-                //iterate until the number becomes zero, NO NEED to go until the last element in roman array
-        while (num >0) {
-            while ( num >= values[i]) {
+        while(num > 0){
+            while(num >= values[i]){
                 num -= values[i];
                 result.append(roman[i]);
             }
