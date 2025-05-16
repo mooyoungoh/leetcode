2305fc397d011f0a20183ac8e43f25d78a8d2918
mooyoungoh <dhandand123@gmail.com>
2025-05-16 11:51:46 -0700

    Time: 98 ms (36.3%), Space: 47.8 MB (29.2%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,11 +1,10 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> wordSet = new HashSet<>(wordList);
-        if(!wordSet.contains(endWord)) return 0;
+        if(!wordList.contains(endWord)) return 0;
 
         Queue<String> q = new LinkedList<>();
         q.add(beginWord);
-        
         Set<String> visited = new HashSet<>();
         visited.add(beginWord);
 
@@ -13,17 +12,17 @@ class Solution {
         while(!q.isEmpty()){
             int size = q.size();
             for(int i = 0; i < size; i++){
-                String word = q.poll();
-                if(word.equals(endWord)) return ans;
-                for(int j = 0; j < word.length(); j++){
-                    char[] wordArr = word.toCharArray();
-                    for(char k = 'a'; k <= 'z'; k++){
-                        if(wordArr[j] == k) continue;
-                        wordArr[j] = k;
-                        String newString = new String(wordArr);
-                        if(!visited.contains(newString) && wordSet.contains(newString)){
-                            q.add(newString);
-                            visited.add(newString);
+                String next = q.poll();
+                if(next.equals(endWord)) return ans;
+                for(int j = 0; j < next.length(); j++){
+                    char[] arr = next.toCharArray();
+                    for(char k = 'a'; k <= 'z' ; k++){
+                        if(arr[j] == k) continue;
+                        arr[j] = k;
+                        String str = new String(arr);
+                        if(!visited.contains(str) && wordSet.contains(str)){
+                            q.add(str);
+                            visited.add(str);
                         }
                     }
                 }
