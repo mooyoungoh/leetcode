a3a013ed1305d228c488c89983a9b7be97e75a05
mooyoungoh <dhandand123@gmail.com>
2025-05-19 15:34:08 -0700

    Time: 34 ms (34.09%), Space: 51.8 MB (44.42%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -12,8 +12,8 @@ class Solution {
                     set.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
-                    while(j < nums.length - 1 && nums[j] == nums[j + 1]) j++;
-                    while(k > 0 && nums[k] == nums[k - 1]) k--;
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
                 }else if(sum < 0){
                     j++;
                 }else{
