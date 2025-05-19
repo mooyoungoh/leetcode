10691de4c1e397bb494e838301911a036e90460b
mooyoungoh <dhandand123@gmail.com>
2025-05-18 22:20:33 -0700

    Time: 0 ms (100%), Space: 41.6 MB (73.54%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -1,41 +1,41 @@
 class Solution {
     public String reorganizeString(String s) {
-        var charCounts = new int[26];
-        for (char c : s.toCharArray()) {
-            charCounts[c - 'a']++;
+        char[] charCount = new char[26];
+        for(char c : s.toCharArray()){
+            charCount[c - 'a']++;
         }
+
         int maxCount = 0, letter = 0;
-        for (int i = 0; i < charCounts.length; i++) {
-            if (charCounts[i] > maxCount) {
-                maxCount = charCounts[i];
+        for(int i = 0; i < charCount.length; i++){
+            if(charCount[i] > maxCount){
+                maxCount = charCount[i];
                 letter = i;
             }
         }
-        if (maxCount > (s.length() + 1) / 2) {
+
+        if(maxCount > (s.length() + 1) / 2){
             return "";
         }
-        var ans = new char[s.length()];
-        int index = 0;
 
-        // Place the most frequent letter
-        while (charCounts[letter] != 0) {
+        char[] ans = new char[s.length()];
+        int index = 0;
+        while(charCount[letter] != 0){
             ans[index] = (char) (letter + 'a');
             index += 2;
-            charCounts[letter]--;
+            charCount[letter]--;
         }
 
-        // Place rest of the letters in any order
-        for (int i = 0; i < charCounts.length; i++) {
-            while (charCounts[i] > 0) {
-                if (index >= s.length()) {
+        for(int i = 0; i < charCount.length; i++){
+            while(charCount[i] > 0){
+                if(index >= s.length()){
                     index = 1;
                 }
                 ans[index] = (char) (i + 'a');
                 index += 2;
-                charCounts[i]--;
+                charCount[i]--;
             }
         }
 
-        return String.valueOf(ans);
+        return new String(ans);
     }
 }
