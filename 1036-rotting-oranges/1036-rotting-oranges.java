2a973bd9eabe438ad5927c7d281f1c987b7bea99
mooyoungoh <dhandand123@gmail.com>
2025-05-20 16:05:21 -0700

    Time: 2 ms (68.51%), Space: 42.2 MB (86.69%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,48 +1,45 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
         int[][] visited = grid;
         Queue<int[]> q = new LinkedList<>();
-        int countFreshOrange = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j] == 2) {
+        int countFresh = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(visited[i][j] == 2){
                     q.offer(new int[] {i, j});
                 }
-                if (visited[i][j] == 1) {
-                    countFreshOrange++;
+                if(visited[i][j] == 1){
+                    countFresh++;
                 }
             }
         }
-        if (countFreshOrange == 0)
-            return 0;
-        if (q.isEmpty())
-            return -1;
-        
+
+        if(countFresh == 0) return 0;
+        if(q.isEmpty()) return -1;
+
         int minutes = -1;
-        int[][] dirs = {{1, 0},{-1, 0},{0, -1},{0, 1}};
-        while (!q.isEmpty()) {
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while(!q.isEmpty()){
             int size = q.size();
-            while (size-- > 0) {
+            for(int i = 0; i < size; i++){
                 int[] cell = q.poll();
                 int x = cell[0];
                 int y = cell[1];
-                for (int[] dir : dirs) {
-                    int i = x + dir[0];
-                    int j = y + dir[1];
-                    if (i >= 0 && i < m && j >= 0 && j < n && visited[i][j] == 1) {
-                        visited[i][j] = 2;
-                        countFreshOrange--;
-                        q.offer(new int[] {i, j});
+                for(int[] dir : dirs){
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];
+                    if(nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && visited[nx][ny] == 1){
+                        visited[nx][ny] = 2;
+                        countFresh--;
+                        q.offer(new int[]{nx, ny});
                     }
                 }
             }
             minutes++;
         }
-        
-        if (countFreshOrange == 0)
-            return minutes;
+
+        if(countFresh == 0) return minutes;
         return -1;
+
     }
 }
