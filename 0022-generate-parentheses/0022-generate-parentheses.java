5ed0b3d7253097375aa234ac1dcb5fef7b9e9ff5
mooyoungoh <dhandand123@gmail.com>
2025-06-12 21:44:05 -0700

    Time: 2 ms (46.95%), Space: 43.4 MB (46.97%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,21 +1,22 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        backtrack(n, 0,0,"", ans);
+        backtrack(n, 0, 0, "", ans);
         return ans;
     }
-    private void backtrack(int n, int numOpen, int numClose, String tmp, List<String> ans){
+
+    public void backtrack(int n, int left, int right, String tmp, List<String> ans){
         //termination
-        if(numOpen == numClose && numOpen == n){
+        if(left == n && right == n){
             ans.add(tmp);
+            return;
         }
-        //recursion
-        if(numOpen < n){
-            backtrack(n, numOpen + 1, numClose, tmp + "(", ans);
+        //backtrack
+        if(left < n){
+            backtrack(n, left + 1, right, tmp + "(", ans);
         }
-        if(numOpen > numClose){
-            backtrack(n, numOpen, numClose + 1, tmp + ")", ans);
+        if(right < left){
+            backtrack(n, left, right + 1, tmp + ")", ans);
         }
-        
     }
 }
