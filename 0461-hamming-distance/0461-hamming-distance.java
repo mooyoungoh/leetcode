5d527c76bed20e7b050dddd5d93d204f978a93af
mooyoungoh <dhandand123@gmail.com>
2025-06-12 22:11:04 -0700

    Time: 0 ms (100%), Space: 40.4 MB (67.71%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,10 +1,10 @@
 class Solution {
     public int hammingDistance(int x, int y) {
         int xor = x ^ y;
-        int count = 0;
+        int ans = 0;
         for(int i = 0; i < 32; i++){
-            count += (xor >> i) & 1;  
+            ans += xor >> i & 1;
         }
-        return count;
+        return ans;
     }
 }
