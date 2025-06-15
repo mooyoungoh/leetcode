ff7df0790e699f2b86c9a618be2d7825990577b1
mooyoungoh <dhandand123@gmail.com>
2025-06-14 21:09:15 -0700

    Time: 3 ms (29.55%), Space: 45 MB (59.09%) - LeetHub

--- 0663-equal-tree-partition/0663-equal-tree-partition.java
+++ 0663-equal-tree-partition/0663-equal-tree-partition.java
@@ -1,12 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     Stack<Integer> seen;
     public boolean checkEqualTree(TreeNode root) {
-        seen = new Stack();
+        seen = new Stack<>();
         int total = sum(root);
         seen.pop();
-        if (total % 2 == 0) {
-            for (int s: seen) {
-                if (s == total / 2) {
+        if(total % 2 == 0){
+            for(int num : seen){
+                if(num == total / 2){
                     return true;
                 }
             }
@@ -14,10 +29,8 @@ class Solution {
         return false;
     }
 
-    public int sum(TreeNode node) {
-        if (node == null) {
-            return 0; 
-        }
+    public int sum(TreeNode node){
+        if(node == null) return 0;
         seen.push(sum(node.left) + sum(node.right) + node.val);
         return seen.peek();
     }
