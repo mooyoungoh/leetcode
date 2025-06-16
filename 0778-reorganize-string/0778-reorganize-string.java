039f2a6b452a1ea1dcb1666b38a83174bca43d88
mooyoungoh <dhandand123@gmail.com>
2025-06-15 17:01:22 -0700

    Time: 0 ms (100%), Space: 41.7 MB (59.58%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -13,18 +13,17 @@ class Solution {
             }
         }
 
-        //terminination
         if(maxCount > (s.length() + 1) / 2){
             return "";
         }
 
         char[] ans = new char[s.length()];
         int index = 0;
-        while(charCount[letter] > 0){
+        while(charCount[letter] > 0 ){
             ans[index] = (char) (letter + 'a');
-            index = index + 2;
+            index += 2;
             charCount[letter]--;
-        }
+        }        
 
         for(int i = 0; i < charCount.length; i++){
             while(charCount[i] > 0){
@@ -32,10 +31,11 @@ class Solution {
                     index = 1;
                 }
                 ans[index] = (char) (i + 'a');
-                index = index + 2;
+                index += 2;
                 charCount[i]--;
             }
         }
+
         return new String(ans);
     }
 }
