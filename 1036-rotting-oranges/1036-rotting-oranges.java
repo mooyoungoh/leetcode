fbdf52588fe140c80bd15dbc93d8967d38663e01
mooyoungoh <dhandand123@gmail.com>
2025-06-15 21:18:53 -0700

    Time: 2 ms (68.3%), Space: 42.4 MB (76.03%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -2,44 +2,42 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int[][] visited = grid;
 
-        int countFresh = 0;
         Queue<int[]> q = new LinkedList<>();
+
+        int freshOrange = 0;
+
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
-                if(visited[i][j] == 2){
+                if(grid[i][j] == 2){
                     q.offer(new int[]{i, j});
                 }
-                if(visited[i][j] == 1){
-                    countFresh++;
+                if(grid[i][j] == 1){
+                    freshOrange++;
                 }
             }
         }
 
-        if(countFresh == 0) return 0;
+        if(freshOrange == 0) return 0;
         if(q.isEmpty()) return -1;
 
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-
-        int minutes = 0;
-        while (!q.isEmpty()) {
+        int[][] dirs ={{1,0}, {-1,0}, {0,1}, {0,-1}};
+        int minutes = -1;
+        while(!q.isEmpty()){
             int size = q.size();
-            boolean newRot = false; // Check if any new orange rotted this minute
-            for (int i = 0; i < size; i++) {
+            for(int i = 0; i < size; i++){
                 int[] cell = q.poll();
-                for (int[] dir : dirs) {
-                    int nx = cell[0] + dir[0];
-                    int ny = cell[1] + dir[1];
-                    if (nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && grid[nx][ny] == 1) {
-                        grid[nx][ny] = 2;
-                        countFresh--;
+                for(int[] dir : dirs){
+                    int nx = dir[0] + cell[0];
+                    int ny = dir[1] + cell[1];
+                    if(nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && visited[nx][ny] == 1){
+                        visited[nx][ny] = 2;
                         q.offer(new int[]{nx, ny});
-                        newRot = true;
+                        freshOrange--;
                     }
                 }
             }
-            if (newRot) minutes++; // Only increment if new rot occurred
+            minutes++;
         }
-        if(countFresh == 0) return minutes;
-        return -1;
+        return freshOrange == 0 ? minutes : -1; 
     }
 }
