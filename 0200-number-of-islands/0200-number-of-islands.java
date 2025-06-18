82ce50e3c6d686e026c39e9a4cd0320a7e58c422
mooyoungoh <dhandand123@gmail.com>
2025-06-17 22:33:38 -0700

    Time: 2 ms (99.84%), Space: 49.4 MB (79.78%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,27 +1,23 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int ans = 0;
-
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
-                    backtrack(grid, i, j);
+                    dfs(grid, i, j);
                     ans++;
                 }
             }
-        }
+        }    
         return ans;
     }
 
-    public void backtrack(char[][] grid, int i, int j){
-        //up
+    public void dfs(char[][]grid, int i, int j){
         grid[i][j] = 'x';
-        if(i - 1 >= 0 && grid[i - 1][j] == '1') backtrack(grid, i - 1, j);
-        //down
-        if(i + 1 < grid.length && grid[i + 1][j] == '1') backtrack(grid, i + 1, j);
-        //left
-        if(j - 1 >= 0 && grid[i][j - 1] == '1') backtrack(grid, i, j-1);
-        //right
-        if(j + 1 < grid[0].length && grid[i][j + 1] == '1')backtrack(grid, i, j + 1);
+        //up
+        if(i > 0 && grid[i-1][j] == '1') dfs(grid, i - 1, j);
+        if(i < grid.length - 1 && grid[i + 1][j] == '1') dfs(grid, i + 1, j);
+        if(j > 0 && grid[i][j-1] == '1') dfs(grid, i, j -1);
+        if(j < grid[0].length - 1 && grid[i][j+1] == '1') dfs(grid, i, j + 1);
     }
 }
