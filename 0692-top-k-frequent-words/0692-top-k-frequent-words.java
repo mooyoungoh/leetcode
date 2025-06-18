9fc6ef3c487d2334e3dffd4b2346d7c7e24cbf54
mooyoungoh <dhandand123@gmail.com>
2025-06-17 23:00:13 -0700

    Time: 7 ms (64.94%), Space: 45.4 MB (7.46%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,17 +1,20 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        Map<String, Integer> map = new HashMap<>(); // word - Freq
+        Map<String, Integer> map = new HashMap<>();
         for(String word : words){
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
-        PriorityQueue<String> pq = new PriorityQueue<>(k, (a,b) -> map.get(a) == map.get(b) ? b.compareTo(a) : map.get(a) - map.get(b));
+        PriorityQueue<String> pq = new PriorityQueue<>(k, (a,b) -> map.get(a) == map.get(b) ? 
+        b.compareTo(a) : map.get(a) - map.get(b)
+        );
+        
         for(String word : map.keySet()){
             pq.offer(word);
             if(pq.size() > k){
                 pq.poll();
             }
         }
-        
+
         List<String> ans = new ArrayList<>();
         while(!pq.isEmpty()){
             ans.add(0, pq.poll());
