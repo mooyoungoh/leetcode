f979eaa2069378f245488b06c88126883ad2db77
mooyoungoh <dhandand123@gmail.com>
2025-07-22 17:11:34 -0700

    Time: 9 ms (45.25%), Space: 44.1 MB (49.64%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("\\s", "");
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            // skip non-alphanumeric left
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            // skip non-alphanumeric right
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if(Character.toUpperCase(s.charAt(left++)) != Character.toUpperCase(s.charAt(right--))) return false;
+        }
+        return true;
+    }
+}
