dd85b40840327a2e2fd0072e28d890fc8540021e
mooyoungoh <dhandand123@gmail.com>
2025-07-25 06:16:29 -0700

    Time: 9 ms (45.01%), Space: 46.8 MB (25.54%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]>ans = new ArrayList<>();
+        List<int[]> ans = new ArrayList<>();
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         for(int i = 1; i < intervals.length; i++){
-            int[] prev = intervals[i-1];
+            int[] prev = intervals[i - 1];
             int[] cur = intervals[i];
             if(prev[1] >= cur[0]){
                 cur[0] = Math.min(prev[0], cur[0]);
                 cur[1] = Math.max(prev[1], cur[1]);
-            }
-            else{
+            }else{
                 ans.add(prev);
             }
         }
@@ -17,11 +16,3 @@ class Solution {
         return ans.toArray(new int[ans.size()][]);
     }
 }
-
-/*
-  prev   cur
-[[1,3],[2,6],[8,10],[15,18]]
-prev[1] > cur[0] // update
-otherwise add into ans
-
-*/
