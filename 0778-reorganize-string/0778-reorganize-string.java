aeabe39311a0a438504545e51b6b228f5d2a75eb
mooyoungoh <dhandand123@gmail.com>
2025-08-22 00:57:39 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -1,16 +1,16 @@
 class Solution {
     public String reorganizeString(String s) {
-        int letter = 0;
         int[] count = new int[26];
         for(char c : s.toCharArray()){
-            count[c-'a']++;
+            count[c - 'a']++;
         }
 
+        int letter = 0;
         int maxCount = -1;
         for(int i = 0; i < count.length; i++){
-            if(count[i] > maxCount) {
-                letter = i;
+            if(maxCount < count[i]){
                 maxCount = count[i];
+                letter = i;
             }
         }
 
@@ -18,16 +18,16 @@ class Solution {
 
         char[] ans = new char[s.length()];
         int index = 0;
-
-        while(count[letter] > 0){
+        
+        while(maxCount > 0){
             ans[index] = (char) (letter + 'a');
             index += 2;
-            count[letter]--;
+            maxCount--;
         }
 
         for(int i = 0; i < count.length; i++){
             while(count[i] > 0){
-                if(index >= s.length()){
+                if(index > s.length() - 1){
                     index = 1;
                 }
                 ans[index] = (char) (i + 'a');
