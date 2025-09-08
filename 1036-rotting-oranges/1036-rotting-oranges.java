1ef587cf298762a479c0c6dc4db52faf2aa0dcfc
mooyoungoh <dhandand123@gmail.com>
2025-09-07 22:59:04 -0700

    Time: 2 ms (66.36%), Space: 42.8 MB (21.68%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,5 +1,6 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+
         int[][] visited = grid;
 
         Queue<int[]> q = new LinkedList<>();
@@ -20,24 +21,28 @@ class Solution {
         if(freshOrange == 0) return 0;
         if(q.isEmpty()) return -1;
 
+        int min = -1;
+
         int[][] dirs ={{1,0}, {-1,0}, {0,1}, {0,-1}};
-        int minutes = -1;
         while(!q.isEmpty()){
             int size = q.size();
             for(int i = 0; i < size; i++){
                 int[] cell = q.poll();
                 for(int[] dir : dirs){
-                    int nx = dir[0] + cell[0];
-                    int ny = dir[1] + cell[1];
-                    if(nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && visited[nx][ny] == 1){
-                        visited[nx][ny] = 2;
-                        q.offer(new int[]{nx, ny});
+                    int x = dir[0] + cell[0];
+                    int y = dir[1] + cell[1];
+
+                    if(x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && 
+                    visited[x][y] == 1){
+                        q.offer(new int[]{x,y});
+                        visited[x][y] = 2;
                         freshOrange--;
                     }
+
                 }
             }
-            minutes++;
+            min++;
         }
-        return freshOrange == 0 ? minutes : -1; 
+        return freshOrange == 0 ? min : -1;
     }
 }
