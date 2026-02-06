3301870bb805060e1080751ff78c5f189200a554
mooyoungoh <dhandand123@gmail.com>
2026-02-06 15:16:17 -0700

    Time: 5 ms (85.73%), Space: 45.8 MB (63.84%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -8,7 +8,7 @@ class Solution {
             }
             ans = Math.max(ans, right - left + 1);
             map.put(s.charAt(right), right + 1);
-        }        
+        }
         return ans;
     }
 }
