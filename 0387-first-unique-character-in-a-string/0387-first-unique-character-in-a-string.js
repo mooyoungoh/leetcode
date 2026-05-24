60eb6bb412806918f83518f08ed59ad7aaf8a89f
mooyoungoh <dhandand123@gmail.com>
2026-05-23 18:24:20 -0700

    Time: 22 ms (90.37%), Space: 57.7 MB (96.59%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let dict = [];
+    let dict = {};
     for(let i = 0; i < s.length; i++){
         dict[s[i]] = (dict[s[i]] || 0) + 1;
     }
-    
+
     for(let i = 0; i < s.length; i++){
-        if(dict[s[i]] === 1){
-            return i;
-        }
+        if(dict[s[i]] === 1) return i; 
     }
+    
     return -1;
-};
+}; 
